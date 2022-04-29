d9cb5020f697c05d9989d90d140411db848e57fb
손가연 <sonc46@naver.com>
2022-04-29 13:03:18 +0900

    update

--- resources/js/history.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	var historyNavi = $('aside'),
-		historyButton = historyNavi.find('button');
-
-	historyButton.click(function () {
-		historyNavi.toggleClass('open');
-		historyNavi.hasClass('open') == true ? historyButton.html('close') : historyButton.html('open');
-	});
-});
